9e8a1c47ef29f589a5d03db6829a58dfd2a7edd5
HUỲNH QUỐC TIẾN <115643869+HuynhQuocTien@users.noreply.github.com>
2025-08-11 14:20:04 +0700

    Create Project

---
+++ src/main/java/com/example/esports_tournament_platform_backend/EsportsTournamentPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.esports_tournament_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsTournamentPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsTournamentPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esports_tournament_platform_backend/EsportsTournamentPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esports_tournament_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsTournamentPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
